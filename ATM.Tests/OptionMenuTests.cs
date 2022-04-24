ff84a040caa884987cfe38023bad80ef466ea351
Marco Barreno <barrendkmarco@gmail.com>
2022-04-23 22:24:04 -0600

    PayTrans Failed

--- ATM.Tests/OptionMenuTests.cs
+++ ATM.Tests/OptionMenuTests.cs
@@ -11,4 +11,11 @@ public class OptionMenuTests
         var payTrans = new PayTrans();
         Assert.AreEqual(790, payTrans.Payment(800, 10));
     }
+    [Test]
+    public void PaytransFailed()
+    {
+        var class1 = new Class1();
+        var payTrans = new PayTrans();
+        Assert.AreEqual(780, payTrans.Payment(800, 10));
+    }
 }
